4984f0a58280ddf58a6bd25a28e9dd610d474b25
HanzZ <hanzz.k@gmail.com>
2013-04-09 18:59:21 +0200

    find pythonqt

---
+++ cmake_modules/PythonQtConfig.cmake
@@ -0,0 +1,25 @@
+find_path(PYTHONQT_INCLUDE_PATH
+NAME PythonQt.h
+PATHS /usr/include/PythonQt /usr/local/include/PythonQt
+)
+
+find_library(PYTHONQT_LIBRARIES
+NAMES PythonQt
+PATH /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64
+)
+
+find_library(PYTHONQTALL_LIBRARIES
+NAMES PythonQt_QtAll
+PATH /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64
+)
+
+if(PYTHONQT_INCLUDE_PATH AND PYTHONQT_LIBRARIES AND PYTHONQTALL_LIBRARIES)
+set(PythonQt_FOUND TRUE)
+set(PYTHONQT_LIBRARIES ${PYTHONQT_LIBRARIES} ${PYTHONQTALL_LIBRARIES})
+else()
+message(FATAL_ERROR "Could not find PythonQt or PythonQt_QtAll")
+set(PythonQt_FOUND FALSE)
+endif()
+
+
+
