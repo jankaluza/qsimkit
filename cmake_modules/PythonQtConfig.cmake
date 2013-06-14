a49ac441e755f65ae7a7b60bda0f7c6c268adf5a
Jan Kaluza <hanzz.k@gmail.com>
2013-06-14 10:09:48 +0200

    Windows compilation fixes

--- cmake_modules/PythonQtConfig.cmake
+++ cmake_modules/PythonQtConfig.cmake
@@ -1,16 +1,16 @@
 find_path(PYTHONQT_INCLUDE_PATH
 NAME PythonQt.h
-PATHS /usr/include/PythonQt /usr/local/include/PythonQt
+PATHS /usr/include/PythonQt /usr/local/include/PythonQt PythonQt/src
 )
 
 find_library(PYTHONQT_LIBRARIES
 NAMES PythonQt
-PATH /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64
+PATH /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64 PythonQt/lib
 )
 
 find_library(PYTHONQTALL_LIBRARIES
 NAMES PythonQt_QtAll
-PATH /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64
+PATH /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64 PythonQt/lib
 )
 
 if(PYTHONQT_INCLUDE_PATH AND PYTHONQT_LIBRARIES AND PYTHONQTALL_LIBRARIES)
