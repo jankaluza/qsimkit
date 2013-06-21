f3db7fdf4d9c385dea1ea4b317ce319a57c6d199
Jan Kaluza <hanzz.k@gmail.com>
2013-06-21 09:35:53 +0200

    Fix Python cmake module

--- cmake_modules/PythonQtConfig.cmake
+++ cmake_modules/PythonQtConfig.cmake
@@ -5,12 +5,12 @@ PATHS /usr/include/PythonQt /usr/local/include/PythonQt PythonQt/src
 
 find_library(PYTHONQT_LIBRARIES
 NAMES PythonQt
-PATH /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64 PythonQt/lib
+PATHS /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64 PythonQt/lib
 )
 
 find_library(PYTHONQTALL_LIBRARIES
 NAMES PythonQt_QtAll
-PATH /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64 PythonQt/lib
+PATHS /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64 PythonQt/lib
 )
 
 if(PYTHONQT_INCLUDE_PATH AND PYTHONQT_LIBRARIES AND PYTHONQTALL_LIBRARIES)
